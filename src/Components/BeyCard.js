78a7993555506b432a275a2a3843f7d4a8de1ebd
NorbertLam <noobertlm@gmail.com>
2019-03-13 11:38:17 -0400

    done

--- src/Components/BeyCard.js
+++ src/Components/BeyCard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class BeyCard extends React.Component {
+
+  handleClick = (event) => {
+    console.log(this)
+    const id = this.props.beyObj.id;
+    this.props.setParentState(id);
+  }
+
+  test = () => {
+    console.log(this.props.parent.logSomething());
+  }
+
   render() {
     return (
       <div>
-        <h3>{/*Bey name goes here*/}</h3>
-        {/*image tag goes here*/}
+        <h3>{this.props.beyObj.name}</h3>
+        <img src={this.props.beyObj.img} onClick={this.test}/>
       </div>
     );
   }
--- src/Containers/BeyContainer.js
+++ src/Containers/BeyContainer.js
@@ -1,13 +1,38 @@
 import React from "react";
+import BeyCard from "../Components/BeyCard"
 
 class BeyContainer extends React.Component {
-  state = {};
+  state = {
+    beyImages: []
+  };
+
+  setBeyContainerState = (id) => {
+    const tmpArr = this.state.beyImages.filter(obj => obj.id !== id)
+    this.setState({
+      beyImages: tmpArr
+    })
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:3001/beys').then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          beyImages: json
+        })
+      })
+  }
+
+  logSomething = () => {
+    return "Hi";
+  }
+  
 
   render() {
+    const beyCardsArr = this.state.beyImages.map(beyImage => <BeyCard key={beyImage.id} beyObj={beyImage} setParentState={this.setBeyContainerState} parent={this}/>)
+
     return (
       <div className="index">
-        <h1>Index</h1>
-        {/*render list*/}
+        {this.state.beyImages.length > 0 && beyCardsArr}
       </div>
     );
   }
